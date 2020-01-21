490fa15600962632b6c8708b96d63bad5baf8722
Kenneth J. Brucker <ken.brucker@action-a-day.com>
2020-01-21 13:43:18 -0800

    ⚡️ Implement zsh shell options
    Setup zsh shell options in new file shell/custom/zsh_options.zsh
    
    Files in shell/custom are autoloaded by Oh My Zhs!

---
+++ shell/zshcustom/zsh_options.zsh
@@ -0,0 +1,50 @@
+#!/bin/zsh
+# zsh_options
+
+# Allow automatic cd to a directory
+setopt AUTO_CD
+
+# Case-insensitive globbing (used in pathname expansion)
+setopt NO_CASE_GLOB
+
+# Enable shell correction
+setopt CORRECT
+setopt CORRECT_ALL
+
+# ==================
+# = Shell History  =
+# ==================
+
+# Increase the maximum number of lines contained in the history file
+# (default is 500)
+SAVEHIST=5000
+
+# Increase the maximum number of commands to remember
+# (default is 500)
+HISTSIZE=2000
+
+# Make some commands not show up in history
+HISTORY_IGNORE="(ls|cd|cd -|pwd|exit|date|* --help)"
+
+# Use extended history
+setopt EXTENDED_HISTORY
+# Share history across multiple zsh sessions
+setopt SHARE_HISTORY
+# Append to history
+setopt APPEND_HISTORY
+# adds commands as they are typed, not at shell exit
+setopt INC_APPEND_HISTORY
+# expire duplicates first
+setopt HIST_EXPIRE_DUPS_FIRST 
+# do not store duplications
+setopt HIST_IGNORE_DUPS
+#ignore duplicates when searching
+setopt HIST_FIND_NO_DUPS
+# removes blank lines from history
+setopt HIST_REDUCE_BLANKS
+# verify history before entering
+setopt HIST_VERIFY
+
+
+
+
