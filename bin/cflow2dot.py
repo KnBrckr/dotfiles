7d8b9d1c2f4b3a73c620be2eed6982b415468002
Kenneth J. Brucker <ken.brucker@action-a-day.com>
2020-05-05 10:51:37 -0700

    cflow2dot.py cleanup
    Update comments, remove unnecessary trim of input line.

--- bin/cflow2dot.py
+++ bin/cflow2dot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 import re
 
@@ -7,19 +8,20 @@ call_stack=[]
 print("digraph {")
 
 for line in sys.stdin:
-    line = line.rstrip()
-    # Line format {  x}    function() <func(args) at file:line>:
+    # Line format: {  n}    function() <func(args) at file:line>:
     match = re.search(r"{(.*)}\s*(\w*)\(\) \<", line)
     if match:
         level = int(match.group(1))
         function = match.group(2)
 
+        # Cleanup call stack so top is caller of this function
         while (len(call_stack) > level ):
             call_stack.pop()
 
         if len(call_stack) > 0:
             print(call_stack[len(call_stack)-1],"->",function)
 
+        # Add function to stack
         call_stack.append(function)
 
 print("}")
