e41e7b3c9f1cb00cc0714a2781d90272f8013676
Kenneth J. Brucker <ken.brucker@action-a-day.com>
2020-11-22 12:20:54 -0800

    git2accurev.py is user specific
    Cleanup git2accurev.py so it can be used by others.

--- bin/git2accurev.py
+++ bin/git2accurev.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 '''
 File: git2accurev.py
 Description: import git repository to Accurev
 
-Usage: python git2accurev.py [git-range]
+Usage: python git2accurev.py git-range
 
 Will update Accurev based on git history specified in git range
 '''
@@ -12,7 +13,7 @@ import tempfile
 import os
 import sys
 
-
+# Retrive get log for the provided range
 def getGitLog(range=''):
     command = ['git', 'log', '--format=%h%n%cd%n%B%x00']
     if range != '':
@@ -21,56 +22,88 @@ def getGitLog(range=''):
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
     if gitLog.returncode != 0:
+        print("git log failed -- exiting")
         exit(1)
     return gitLog.stdout.decode().split('\0')
 
 
 def checkout(commit):
     if subprocess.run(['git', 'checkout', commit]).returncode != 0:
+        print("git checkout failed -- exiting")
         exit(1)
 
 
+# Use AccuRev stat command to retrieve a list of files matching requested status
+# -x => Files to be added
+# -mD => Files to be undeleted
+# -m => Modified files
+# -M => Files to be deleted
 def ac_stat(type):
     stat = subprocess.run(['accurev.exe', 'stat', type, '-fl'],
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE)
     if stat.returncode != 0:
+        print("accurev stat failed -- exiting")
         exit(1)
     return stat.stdout.decode()
 
-
+# Run the requested accurev command
 def ac_run(stat_option, command, comment_file):
+
+    # Get list of files to be included in operation
     files = ac_stat(stat_option)
     if (len(files) > 0):
+        # Create temp file containing list of files
         fl = mktemp(files)
         result = subprocess.run(['accurev.exe', command, '-c', '@' +
                                 comment_file.replace('/mnt/c', ''), '-l', fl.replace('/mnt/c', '')])
         os.unlink(fl)
-        # accurev undefunct will fail with a CRC error if the returning file has been modified.
+        # accurev undefunct will fail with a CRC error if the file has been modified.
         if command != 'undefunct' and result.returncode != 0:
+            print("accurev command ", command, " failed -- exiting")
             exit(1)
 
+# Create a temp file with the provided contents
 def mktemp(contents):
-    temp_fd, temp_path = tempfile.mkstemp(dir='/mnt/c/Users/kbrucker/tmp')
+    temp_fd, temp_path = tempfile.mkstemp(dir=os.environ.get('WINTMP'))
     with open(temp_path, "w") as f:
         f.write(contents)
     os.close(temp_fd)
     return temp_path
 
-
+# Apply changes from a git commit to AccuRev preserving the git comment
+# One git commit may translate into up to four separate AccuRev commits
+# as AccuRev treats as separate actions adding a new file (add), undeleting 
+# a file (undefunct), updating an existing file (keep), or deleting a file
+# (defunct)
 def migrate(comment_file):
     ac_run('-x', 'add', comment_file)
     ac_run('-mD', 'undefunct', comment_file)
     ac_run('-m', 'keep', comment_file)
     ac_run('-M', 'defunct', comment_file)
 
+#
+# Main Routine
+#
+
+# A git range must be provided
 if len(sys.argv) > 1:
     range = sys.argv[1]
 else:
-    range = ''
+    print("usage: %s <git-range>", sys.argv[0])
+    print("Please specify git commit range to add to accurev")
+    exit(1)
 
+# WINTMP used to create tmp files access by windows accurev.exe CLI.
+if os.environ.get('WINTMP') == None:
+    print("Please define environment variable WINTMP to reference temp directory in Windows user environment")
+    print("eg. export WINTMP=/mnt/c/Users/<username>/tmp")
+    exit(1)
+
+# Collect git log for the provided range
 log = getGitLog(range)
 
+# Reverse the log order to start from the oldest commit
 for commit in reversed(log):
     lines = commit.strip().splitlines()
     if len(lines) == 0:
