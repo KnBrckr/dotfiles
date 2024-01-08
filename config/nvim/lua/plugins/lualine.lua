8b0ea68623ab5814151bd4c0c077282d8aa7b3b2
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2024-01-08 08:59:30 -0800

    nvim: Use full path for filename
    Update lualine configuration to include full path to file in current
    buffer.

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,13 @@ return {
 	config = function()
 		require("lualine").setup {
 			sections = {
+				lualine_c = {
+					{
+						'filename',
+						newfile_status = true,
+						path = 3,
+					}
+				},
 				lualine_z = { 'location', "os.date('%H:%M')" },
 			},
 		}
