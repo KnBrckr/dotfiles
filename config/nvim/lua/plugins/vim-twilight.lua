3ef50872e71edce855af5c778ccfe75d43e61a47
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2024-06-07 22:50:52 -0700

    Add vim-twilight
    Auto-dim inactive sections of the screen. Auto-enabled by Zen-mode.
    https://github.com/folke/twilight.nvim

---
+++ config/nvim/lua/plugins/vim-twilight.lua
@@ -0,0 +1,10 @@
+-- Dim inactive sections of the screen
+-- Auto-enabled by Zen-Mode
+return {
+  "folke/twilight.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
