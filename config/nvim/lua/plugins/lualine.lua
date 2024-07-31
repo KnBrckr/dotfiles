56097a7aef76998b040447f0f62f73ba05ce8878
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2024-07-30 22:42:30 -0700

    nvim: add word count to text files
    Adds a wordcount to lualine for asciidoc, markdown and text files

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,21 @@
 -- status line
+
+-- Add a word count to status line for text files
+local function getWords()
+	local ft = vim.bo.filetype
+	-- Display word-count for text documents
+	if ft:match "^asciidoc" or ft == 'markdown' or ft == 'text'  or ft == 'org' then
+		local mode = vim.fn.mode()
+		-- If visual-mode
+		if mode:match "^[vV\22]" then
+			return vim.fn.wordcount().visual_words .. " words"
+		else
+			return vim.fn.wordcount().words .. " words"
+		end
+	end
+	return ""
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
@@ -11,7 +28,10 @@ return {
 						path = 3,
 					}
 				},
-				lualine_z = { 'location', "os.date('%H:%M')" },
+				lualine_z = {
+					{ getWords, draw_empty = false, },
+					'location',
+					"os.date('%H:%M')" },
 			},
 		}
 	end,
