4922e26f36a3e68322783e268a2866eb8dfe5fa9
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2024-08-25 10:43:02 -0700

    nvim: Auto-enable Pencil based on file type
    Auto-configure Pencil when file type is one of
    - asciidoc
    - text
    - markdown

--- config/nvim/lua/plugins/vim-pencil.lua
+++ config/nvim/lua/plugins/vim-pencil.lua
@@ -1,4 +1,16 @@
 -- VIM as a writing tool
+local file_types = { "asciidoc", "text", "markdown" }
+
 return {
 	"preservim/vim-pencil",
+	ft = file_types,
+	config = function()
+		local group = vim.api.nvim_create_augroup("pencil", { clear = true })
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = file_types,
+			command = "call pencil#init()",
+			group = group
+		}
+		)
+	end,
 }
