ca07672fbab15c75edb2e88bf2efba8ed7423ab1
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2024-10-12 22:24:13 -0700

    nvim: Use soft wrap for asciidoc files
    To allow use of vim-pencil, use soft wrap for adoc files to avoid
    reformatting of lines with hard-wraps.

--- config/nvim/lua/plugins/vim-pencil.lua
+++ config/nvim/lua/plugins/vim-pencil.lua
@@ -1,16 +1,30 @@
 -- VIM as a writing tool
-local file_types = { "asciidoc", "text", "markdown" }
+
+-- Map file types to desired wrap mode
+local file_table = {
+	asciidoc = "soft",
+	text = "hard",
+	markdown = "hard",
+}
+
+-- List of file types for lazy loading
+local file_types = {}
+for type, _ in pairs(file_table) do
+	table.insert(file_types, type)
+end
 
 return {
 	"preservim/vim-pencil",
 	ft = file_types,
 	config = function()
 		local group = vim.api.nvim_create_augroup("pencil", { clear = true })
-		vim.api.nvim_create_autocmd('FileType', {
-			pattern = file_types,
-			command = "call pencil#init()",
-			group = group
-		}
-		)
+		for type, wrap in pairs(file_table) do
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = type,
+				command = "call pencil#init({'wrap': '" .. wrap .. "' })",
+				group = group
+			}
+			)
+		end
 	end,
 }
