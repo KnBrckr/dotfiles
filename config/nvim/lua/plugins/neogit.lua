b424c0759e4ca9ebfe6fd0375336c132859306c5
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2024-10-19 08:39:59 -0700

    nvim: replace vim-fugitive with neogit
    Add https://github.com/NeogitOrg/neogit to nvim configuration.
    
    Neogit is written in lua and provides similar functionality to
    vim-fugitive written in vimscript.
    
    Overall functionality appears better in neogit.

---
+++ config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,19 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    -- Only one of these is needed.
+    "nvim-telescope/telescope.nvim", -- optional
+    -- "ibhagwan/fzf-lua",              -- optional
+    -- "echasnovski/mini.pick",         -- optional
+  },
+  config = function ()
+		local neogit = require("neogit")
+		neogit.setup {
+			-- Use unicode (vs. ascii) for generating log graphs
+			graph_style = "unicode",
+		}
+  end
+}
