c566f6981e79830918493fd482f67298f92a1f5d
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2025-04-04 10:27:35 -0700

    fix(nvim): Use tokyonight theme in lualine
    Update lualine configuration to use tokyonight theme per guidance
    at https://github.com/folke/tokyonight.nvim?tab=readme-ov-file#lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -20,6 +20,9 @@ return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
 		require("lualine").setup {
+			options = {
+				theme = "tokyonight",
+			},
 			sections = {
 				lualine_c = {
 					{
