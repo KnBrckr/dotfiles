c0ff50459bdafd171ef6862c7042f5c2bf7c2b20
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2025-07-27 21:57:27 -0700

    nvim: Use client:supports_method
    client.supports_method() is deprecated. Update usage to
    client:supports_usage() instead.

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -15,7 +15,7 @@ return {
 				-- Buffer local mappings.
 				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				-- See `:help vim-api` for list of methods
-				if client.supports_method('textDocument/publishDiagnostics') then
+				if client:supports_method('textDocument/publishDiagnostics') then
 					-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 					vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev({ float = true }) end,
 						{ buffer = true, desc = 'Diagnostics: prev' })
@@ -27,32 +27,32 @@ return {
 						{ buffer = true, desc = "Add diagnostics to location list" })
 				end
 
-				if client.supports_method('textDocument/declaration') then
+				if client:supports_method('textDocument/declaration') then
 					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = true, desc = "Jump to Declaration" })
 				end
 
-				if client.supports_method('textDocument/definition') then
+				if client:supports_method('textDocument/definition') then
 					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = true, desc = "Jump to Definition" })
 				end
 
-				if client.supports_method('textDocument/typeDefinition') then
+				if client:supports_method('textDocument/typeDefinition') then
 					vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition,
 						{ buffer = true, desc = "Jump to Type definition" })
 				end
 
-				if client.supports_method('textDocument/implementation') then
+				if client:supports_method('textDocument/implementation') then
 					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = true, desc = "Jump to Implementation" })
 				end
 
-				if client.supports_method('textDocument/references') then
+				if client:supports_method('textDocument/references') then
 					vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = true, desc = "Jump to References" })
 				end
 
-				if client.supports_method('textDocument/signatureHelp') then
+				if client:supports_method('textDocument/signatureHelp') then
 					vim.keymap.set('n', 'KK', vim.lsp.buf.signature_help, { buffer = true, desc = "Show Signature Help" })
 				end
 
-				if client.supports_method('workspace/workspaceFolders') then
+				if client:supports_method('workspace/workspaceFolders') then
 					vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder,
 						{ buffer = true, desc = "Add workspace folder" })
 					vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
@@ -62,15 +62,15 @@ return {
 					end, { buffer = true, desc = "List workspace folders" })
 				end
 
-				if client.supports_method('textDocument/rename') then
+				if client:supports_method('textDocument/rename') then
 					vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, { buffer = true, desc = "Rename symbol" })
 				end
 
-				if client.supports_method('textDocument/codeAction') then
+				if client:supports_method('textDocument/codeAction') then
 					vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, { buffer = true, desc = "Code Action" })
 				end
 
-				if client.supports_method('textDocument/formatting') then
+				if client:supports_method('textDocument/formatting') then
 					vim.keymap.set('n', '<leader>f', function()
 						vim.lsp.buf.format { async = true }
 					end, { buffer = true, desc = "Format" })
