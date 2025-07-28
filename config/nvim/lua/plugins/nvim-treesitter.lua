9512177ad706d5ee1267709ba3ca7577f7f5eec8
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2025-07-27 21:40:35 -0700

    nvim: Update telescope, nvim-treesitter
    Update to nvim-treesitter also required a change to telescope version.
    In both cases, the branch used needed to be changed.
    
    Per https://github.com/nvim-telescope/telescope.nvim/issues/3487,
    telescope 0.1.x branch is no longer maintained (even though the
    readme in master still directs usage to it.) Fixes are now made
    available through master branch.
    
    Over in nvim-treesitter land, the master branch is now frozen,
    be be replaced with the main branch.

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,53 +1,13 @@
 -- Treesitter based syntax highlights
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	branch = "main",
+	build = ":TSUpdate",
 	init = function()
 		-- By default use treesitter for folding
 		vim.opt.foldmethod = "expr"
 		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 		vim.opt.foldlevel = 1
-	end,
-	config = function()
-		require('nvim-treesitter.configs').setup({
-			highlight = {
-				enable = true,
-				-- Setting this to true or a list of languages will run `:h syntax` and tree-sitter at the same time.
-				additional_vim_regex_highlighting = false,
-			},
-			-- Install parsers asynchronously (only applied to 'ensure_installed')
-			sync_install = false,
-			-- Auto-install missing parsers (needs tree-sitter command locally available)
-			auto_install = false,
-			ensure_installed = {
-				'awk',
-				'bash',
-				'c',
-				'cmake',
-				'diff',
-				'dockerfile',
-				'dot',
-				'git_config',
-				'git_rebase',
-				'gitattributes',
-				'gitcommit',
-				'gitignore',
-				'graphql',
-				'json',
-				'lua',
-				'make',
-				'markdown',
-				'ninja',
-				'perl',
-				'python',
-				'regex',
-				'sql',
-				'todotxt',
-				'vim',
-				'vimdoc',
-				'yaml',
-			},
-			ignore_install = {
-			},
-		})
 	end,
 }
