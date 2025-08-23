e99609ab1905c61eb70107745b912ede06e89cc3
Ken Brucker <kbrucker@ilts.com>
2025-08-23 12:10:16 -0700

    fix(nvim): Provide default LSP environment
    Include a collection of default LSP parsers

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,9 +1,23 @@
 -- Mason LSP package manager
--- https://github.com/mason-org/mason.nvim
+-- https://github.com/williamboman/mason.nvim
 -- https://github.com/mason-org/mason-lspconfig.nvim
 return {
 	"mason-org/mason-lspconfig.nvim",
-	opts={},
+	config = function()
+		local mason_lspconfig = require('mason-lspconfig')
+
+		mason_lspconfig.setup {
+			ensure_installed = {
+				"bashls", -- bash language server
+				"clangd", -- C language server
+				"cmake", -- cmake language server
+				"jsonls", -- JSON language server
+				"lua_ls", -- Lua language server
+				"pylsp", -- Python language server
+				"yamlls" -- YAML language server
+			},
+		}
+	end,
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		{
