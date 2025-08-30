e9478f5a6bfd7e41b3bc28c3e6e2c9ddf8052962
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2025-08-29 21:16:19 -0700

    fix(nvim): vim.loop is deprecated
    Switch to vim.uv, vim.loop is deprecated.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -109,7 +109,7 @@ vim.g.loaded_netrwPlugin = 1
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
