dce1fc9e5b7ccd2781fc20fb3bf928f338805d05
Ken Brucker <232873+KnBrckr@users.noreply.github.com>
2025-09-14 15:52:30 -0700

    fix(nvim): Prototype update to neogen for file headers

--- config/nvim/lua/plugins/neogen.lua
+++ config/nvim/lua/plugins/neogen.lua
@@ -27,8 +27,38 @@ return {
 	},
 	config = function()
 		local neogen = require('neogen')
+		local item = require('neogen.types.template').item
+		local doxygen_setup = {
+			{ nil,            "/**",                                                    { no_results = true, type = { "func", "file", "class" } } },
+			{ nil,            " * @file ",                                              { no_results = true, type = { "file" } } },
+			{ nil,            " * @brief $1",                                     { no_results = true, type = { "file" } } },
+			{ nil,            " * ",                                                    { no_results = true, type = { "file" } } },
+			{ nil,            " * Copyright © 2024 COMPANY_NAME. All rights reserved.", { no_results = true, type = { "file" } } },
+			{ nil,            " */",                                                    { no_results = true, type = { "func", "file", "class" } } },
+			{ nil,            "",                                                       { no_results = true, type = { "file" } } },
+
+			{ nil,            "/**",                                                    { type = { "func", "class", "type" } } },
+			{ item.ClassName, " * @class %s",                                           { type = { "class" } } },
+			{ item.Type,      " * @typedef %s",                                         { type = { "type" } } },
+			{ nil,            " * @brief $1",                                           { type = { "func", "class", "type" } } },
+			{ nil,            " *",                                                     { type = { "func", "class", "type" } } },
+			{ item.Tparam,    " * @tparam %s $1" },
+			{ item.Parameter, " * @param %s $1" },
+			{ nil,            " *",                                                     { type = { "func", "class", "type" } } },
+			{ item.Return,    " * @return $1" },
+			{ nil,            " */",                                                    { type = { "func", "class", "type" } } },
+
+		}
 		neogen.setup {
 			enabled = true,
+			languages = {
+				c = {
+					template = {
+						annotation_convention = "custom_doxygen",
+						custom_doxygen = doxygen_setup,
+					},
+				},
+			},
 		}
 	end,
 }
